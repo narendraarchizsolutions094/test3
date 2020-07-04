7dd46a6e71a640d5c0088db701f6faba167e19c6
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-07-04 14:59:23 -0700

    multiple drop issue fixed

--- application/views/enquiry_n.php
+++ application/views/enquiry_n.php
@@ -582,6 +582,58 @@ input[name=lead_stages]{
 		</div>
 	</div>
 </div>
+
+
+        
+<div id="dropEnquiry" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+ 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Drop Lead</h4>
+      </div>
+      <div class="modal-body">
+                        
+                                       
+           
+              <div class="row">
+                
+                <div class="form-group col-sm-12">  
+                <label>Drop Reason</label>                  
+                <select class="form-control"  name="drop_status">                    
+                <?php foreach ($drops as $drop) {   ?>
+                <option value="<?php echo $drop->d_id; ?>"><?php echo $drop->drop_reason; ?></option>
+                <?php } ?>                                             
+                </select> 
+                </div>
+                
+                <div class="form-group col-sm-12"> 
+                  <label>Drop Reason*</label>
+                  <input class="form-control" name="reason" type="text" required="">  
+                </div> 
+                
+              </div>          
+              <div class="col-12" style="padding: 0px;">
+                <div class="row">              
+                  <div class="col-12" style="text-align:center;">                                                
+                    <button class="btn btn-success" type="button" onclick="drop_enquiry()">Save</button>            
+                  </div>
+                </div>                                   
+              </div> 
+                  
+         
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
 <div id="sendsms" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -887,54 +939,7 @@ for (var i = 0; i < checkboxes.length; i++) {
 </script>
 <!---------------------------- DROP Lead -------------------------------->
 
-        
-<div id="dropEnquiry" class="modal fade" role="dialog">
-  <div class="modal-dialog">
- 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Drop Lead</h4>
-      </div>
-      <div class="modal-body">
-                        
-                                       
-           
-              <div class="row">
-                
-                <div class="form-group col-sm-12">  
-                <label>Drop Status</label>                  
-                <select class="form-control"  name="drop_status">                    
-                <?php foreach ($drops as $drop) {   ?>
-                <option value="<?php echo $drop->d_id; ?>"><?php echo $drop->drop_reason; ?></option>
-                <?php } ?>                                             
-                </select> 
-                </div>
-                
-                <div class="form-group col-sm-12"> 
-                  <label>Drop Reason*</label>
-                  <input class="form-control" name="reason" type="text" required="">  
-                </div> 
-                
-              </div>          
-              <div class="col-12" style="padding: 0px;">
-                <div class="row">              
-                  <div class="col-12" style="text-align:center;">                                                
-                    <button class="btn btn-success" type="button" onclick="drop_enquiry()">Save</button>            
-                  </div>
-                </div>                                   
-              </div> 
-                  
-         
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
 
-  </div>
-</div>
   
 
 
