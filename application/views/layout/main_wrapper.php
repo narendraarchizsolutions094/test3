9cb25314bdf8440386faac85acc67f125850df9d
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-07-27 11:09:02 -0400

    rule for all company

--- application/views/layout/main_wrapper.php
+++ application/views/layout/main_wrapper.php
@@ -787,9 +787,7 @@ if($root=='https://student.spaceinternationals.com'){  ?>
                      </a>
                      <ul class="treeview-menu <?php echo (($segment1 == "lead" && $segment2 != "index" && $segment2 != "lead_details") ?"menu-open":'') ?>">
                         <li class="<?php echo (in_array($segment2,array('stage')) ?"active":'') ?>">
-                        <?php if($this->session->companey_id == 1) { ?>
                           <a href="<?php echo base_url(); ?>leadRules"> <?php echo display('lead_rules'); ?> </a>
-                        <?php }?>
                         </li>
 
                         <li class="<?php echo (in_array($segment2,array('stage')) ?"active":'') ?>">
