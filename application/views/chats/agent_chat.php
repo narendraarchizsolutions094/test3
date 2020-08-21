aeda6cc5471bd98aaff913e35a037942cc72057a
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-08-21 11:37:57 -0500

    chat trans

--- application/views/chats/agent_chat.php
+++ application/views/chats/agent_chat.php
@@ -592,7 +592,7 @@ function after_load(){
 					    return letter.toUpperCase();
 					});
 	      	if (change.type === 'added') {
-	    		html = '<li class="person" data-chat="'+msg1_data.uid+'"><img src="https://avatars0.githubusercontent.com/u/10263615?v=4" alt=""/><span class="name">'+name+' ['+msg1_data.uid+']<a id="'+msg1_data.uid+'_unread" class="btn btn-sm btn-circle btn-default"></a></span><span class="time">'+msg1_data.time+'</span><span class="preview" ></span></li>';
+	    		html = '<li class="person" data-chat="'+msg1_data.uid+'"><img src="https://avatars0.githubusercontent.com/u/10263615?v=4" alt=""/><span class="name">'+name+' ['+msg1_data.uid+']<a id="'+msg1_data.uid+'_unread" class="btn btn-sm btn-circle btn-default" style="position: absolute;left: 0;margin-left: 56px;margin-top: -10px;width: 6%;"></a></span><span class="time">'+msg1_data.time+'</span><span class="preview" ></span></li>';
 	            $(".people").append(html);
 	            $(".right>.top").after('<div class="chat" data-chat="'+msg1_data.uid+'"></div>');
 
