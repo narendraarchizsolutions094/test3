d40536b1dd349dc55c3bc21b95ed2bb80ed4e7f1
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-08-21 01:06:47 -0500

    multi select issue

--- application/views/enquiry/enquiry_tab_content.php
+++ application/views/enquiry/enquiry_tab_content.php
@@ -8,8 +8,8 @@ if (user_access(450)) { ?>
 <?php
 }
 ?>
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+<script src="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.jquery.min.js"></script>
+<link href="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.min.css" rel="stylesheet"/>
 
 <?php
    if ($tid == 1) {      
