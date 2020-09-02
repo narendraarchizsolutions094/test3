7f08416e805c4ac8933fcc4378fa2759819a7655
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-09-02 04:30:15 -0500

    Revert "revert"
    This reverts commit 3d27f6a246382f5f7234e66a1fc4ac1eaf078b48.

--- application/views/layout/main_wrapper.php
+++ application/views/layout/main_wrapper.php
@@ -26,6 +26,8 @@
            $segment3 = $this->uri->segment(3);
    ?>
 
+  
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -1944,7 +1946,7 @@ $("#anch_notification_dropdown").on('click',function(){
 <script>
 $('#something').click(function() {
 //alert('hi');  
-    document.location = '<?php echo base_url('dashboard/menu_style'); ?>';
+    //document.location = '<?php //echo base_url('dashboard/menu_style'); ?>';
 } );
 </script>
   <!-- Insert these scripts at the bottom of the HTML, but before you use any Firebase services -->
