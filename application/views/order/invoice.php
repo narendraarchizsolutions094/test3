b8b52111ed5313750992656f890d12b2d8552e3f
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-09-18 06:26:27 -0500

    tax in invoice

--- application/views/order/invoice.php
+++ application/views/order/invoice.php
@@ -92,7 +92,7 @@
 														<td class="text-center" style="width: 1%"><?php echo $pending; ?></td>
 														<td class="text-right"><i class ="fa fa-rupee"></i> <i ><?php echo $price = $ord->price; ?></td>
 														<!-- <td class="text-right"><?php echo $oprice = $ord->other_price; ?></td> -->
-														<td class="text-right"><?php echo  (!empty($ord->gst)) ? $ord->gst."%" : ""; ?></td>
+														<td class="text-right"><?php echo  (!empty($ord->tax)) ? $ord->tax."%" : ""; ?></td>
 														<td class="text-right"><?php echo $ord->offer; ?></td>
 														<td class="text-right"><i class ="fa fa-rupee"></i>
 															<?php 
