e92ae86dda614082c8ee9d8e761ede8e1a25246c
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-09-21 05:59:10 -0500

    cart

--- application/views/sell/product-list.php
+++ application/views/sell/product-list.php
@@ -365,7 +365,7 @@
 			type 	: "post",
 			data 	: {
 				product:$(this).data("prodid"),
-				qty:$(this).first().html(),
+				qty:$(this).first().next().html(),
 				disc:$(this).closest("li").find(".tot-price").val()
 			},
 			success	: function(resp){
