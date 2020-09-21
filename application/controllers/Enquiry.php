e3619ae62fd3ffc42afe3d724718aa596555e3df
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-09-21 02:59:19 -0500

    institute tab data

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -3188,8 +3188,9 @@ function upload_enquiry() {
             }
         } 
 
-    public function get_institute_tab_content(){
+    public function get_institute_tab_content($enquiry_id){
         $data = array();
+        $data['details'] = $this->Leads_Model->get_leadListDetailsby_id($enquiry_id);           
         $data['institute_list'] = $this->Institute_model->institutelist_by_country($data['details']->enq_country); 
         $data['course_list'] = $this->Leads_Model->get_course_list();
         $data['institute_app_status'] = $this->Institute_model->get_institute_app_status();
