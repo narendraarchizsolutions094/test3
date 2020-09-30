102cc292940469a76812993b32b60e41ef4abd9c
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-09-30 02:51:12 -0500

    rules

--- application/models/Rule_model.php
+++ application/models/Rule_model.php
@@ -1,9 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Rule_model extends CI_Model {
 	
-	public function get_rule($id){
+	public function get_rule($id,$comp_id=0){
+        if ($comp_id == 0) {
+            $comp_id    =   $this->session->companey_id;
+        }
 		$this->db->where('id',$id);
-		$this->db->where('comp_id',$this->session->companey_id);
+		$this->db->where('comp_id',$comp_id);
 		return $this->db->get('leadrules')->row_array(); 
 	}
 
@@ -27,7 +30,7 @@ class Rule_model extends CI_Model {
             $user_id = $this->session->user_id;
         }
 		//$this->load->model('rule_model');
-		$rule_data    =   $this->get_rule($id);        
+		$rule_data    =   $this->get_rule($id,$comp_id);        
         $affected = 0;
         if (!empty($rule_data)) {
             if (!empty($rule_data['rule_sql']) && $rule_data['status'] == 1) {
