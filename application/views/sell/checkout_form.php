c82804668be58672987e951719c4c8211fd3af85
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-10-10 09:41:36 -0500

    test

--- application/views/sell/checkout_form.php
+++ application/views/sell/checkout_form.php
@@ -21,7 +21,7 @@
   background: -o-linear-gradient(top left, #5195A8 0%, #70EAFF 100%);
   background: linear-gradient(to bottom right, #5195A8 0%, #70EAFF 100%);
   border-radius:5px 5px 0 0 ;
-  padding: 20px;
+  padding: 7px;
   color: #f6f6f6;
 }
 h2{
@@ -34,7 +34,7 @@ h2{
 
 .Yorder{
   margin-top: 15px;
-  height: 650px;
+ /* height: 650px;*/
   padding: 20px;
   border: 1px solid #dadada;
 }
@@ -79,7 +79,7 @@ p{
   padding: 10px;
   border: none;
   border-radius: 30px;
-  background: #52ad9c;
+  background: #53d4fa;
   color: #fff;
   font-size: 15px;
   font-weight: bold;
@@ -93,142 +93,200 @@ p{
 <div class="">
   <div class="checkout-title">
     <a href="<?=base_url('buy');?>" class="btn btn-md pull-right">Back</a>
-      <h2>Product Order Form</h2>
-
+      <h2>Checkout Form</h2>
   </div>
-<div class="row">
-  <br>
-<div class="col-md-8">
-    <div class="row Yorder">
-        <div class="col-md-12">
-            <div class="form-group">        
-                <label>Full Name <i class="required">*</i></label>
-                <input type="text" name="fname" class="form-control" required value="<?=$user_row->s_display_name.' '.$user_row->last_name?>">
-            </div>        
-        </div>       
-        
-        <div class="col-md-12">
-           <div class="form-group">        
-            <label>Billing Address <i class="required">*</i></label>
-            <input type="text" name="address" placeholder="House number and street name with zipcode" required class="form-control" value="<?=$user_row->add_ress?>">
-          </div>          
-        </div>
-<div class="col-md-12">   
-<label for="chkshipping">
-    <input type="checkbox" id="chkshipping" />
-    SHIP TO A DIFFERENT ADDRESS?
-</label>
-</div>
-<div class="col-md-12" id="dvship" style="display: none">
-   <div class="col-md-12">
-           <div class="form-group">        
-            <label>Shipping Address <i class="required">*</i></label>
-            <input type="text" name="ship_address" placeholder="House number and street name with zipcode" required class="form-control">
-          </div>          
-        </div>    
+  <div class="row">
+    <br>
+      <div class="col-md-8">
+          <div class="panel panel-default" style="border: 4px solid #53d4fa;">
+            <div class="panel-heading no-print" style="background-color: #53d4fa;">
+                Billing Details 
+            </div>
+            <div class="panel-body panel-form" >
+              <div class="row ">
+                  <div class="col-md-12">
+                      <div class="form-group">        
+                          <label>Full Name <i class="required">*</i></label>
+                          <input type="text" name="fname" class="form-control" required value="<?=$user_row->s_display_name.' '.$user_row->last_name?>">
+                      </div>        
+                  </div>       
                   
-</div>
+                  <div class="col-md-12">
+                     <div class="form-group">        
+                      <label>Address <i class="required">*</i></label>
+                      <input type="text" name="address" placeholder="House number and street name with zipcode" required class="form-control" value="<?=$user_row->add_ress?>">
+                    </div>          
+                  </div>
 
-<div class="col-md-6">
-<div class="form-group">        
-<label> State <i class="required">*</i></label>
-<select name="state" id="state" class="form-control" required>
-<option value="">Select State</option>
-<?php foreach($user_state as $state){ ?>
-  <option value="<?php echo $state->id; ?>"><?php echo $state->state; ?></option>
-<?php } ?>
-</select> 
-</div>
-</div>        
-<div class="col-md-6">
-<div class="form-group">        
-<label> City <i class="required">*</i></label>
-<select name="city" id="city" class="form-control" required>
-<option value="">Select City</option>
-<?php foreach($user_city as $city){ ?>
-  <option value="<?php echo $city->id; ?>"><?php echo $city->city; ?></option>
-<?php } ?>
-</select> 
-</div>
-</div>
-       
-        <div class="col-md-6">
-          <div class="form-group">        
-          <label>Postcode / ZIP <i class="required">*</i></label>
-          <input type="text" name="pincode" class="form-control" required value="<?=!empty($user_meta['postal_code'])?$user_meta['postal_code']:''?>"> 
-        </div>
-        </div>        
-        <div class="col-md-6">
-            <div class="form-group">        
-
-          <label>Mobile no. <i class="required">*</i></label>
-          <input type="tel" name="phone" class="form-control" required value="<?=$user_row->s_phoneno?>"> 
-        </div>
-        </div>        
-        <div class="col-md-6">
-            <div class="form-group">    
-              <label>Email Address <i class="required">*</i></label>
-              <input type="email" name="email" class="form-control" required value="<?=$user_row->s_user_email?>"> 
-              
+                  <div class="col-md-6">
+                    <div class="form-group">        
+                      <label> State <i class="required">*</i></label>
+                      <select name="state" id="state" class="form-control" required>
+                        <option value="">Select State</option>
+                        <?php foreach($user_state as $state){ ?>
+                          <option value="<?php echo $state->id; ?>"><?php echo $state->state; ?></option>
+                        <?php } ?>
+                      </select> 
+                    </div>
+                  </div>        
+                  <div class="col-md-6">
+                    <div class="form-group">        
+                    <label> City <i class="required">*</i></label>
+                    <select name="city" id="city" class="form-control" required>
+                      <option value="">Select City</option>
+                      <?php foreach($user_city as $city){ ?>
+                        <option value="<?php echo $city->id; ?>"><?php echo $city->city; ?></option>
+                      <?php } ?>
+                    </select> 
+                    </div>
+                  </div>
+                 
+                  <div class="col-md-6">
+                    <div class="form-group">        
+                      <label>Postcode / ZIP <i class="required">*</i></label>
+                      <input type="text" name="pincode" class="form-control" required value="<?=!empty($user_meta['postal_code'])?$user_meta['postal_code']:''?>"> 
+                    </div>
+                  </div>        
+                  <div class="col-md-6">
+                      <div class="form-group">
+                      <label>Mobile no. <i class="required">*</i></label>
+                      <input type="tel" name="phone" class="form-control" required value="<?=$user_row->s_phoneno?>"> 
+                    </div>
+                  </div>        
+                  <div class="col-md-6">
+                      <div class="form-group">    
+                        <label>Email Address <i class="required">*</i></label>
+                        <input type="email" name="email" class="form-control" required value="<?=$user_row->s_user_email?>"> 
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="form-group">    
+                        <label>GSTIN No (optional)</label>
+                        <input type="text" name="gstin" class="form-control" value="<?=!empty($user_meta['gstin'])?$user_meta['gstin']:''?>"> 
+                        <br>
+                      </div>
+                  </div>
+                  <label for="chkshipping" style="float: right;">
+                    <input type="checkbox" id="chkshipping" />
+                      SHIP TO A DIFFERENT ADDRESS?
+                  </label>
+              </div>
             </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">    
-              <label>GSTIN No (optional)</label>
-              <input type="text" name="gstin" class="form-control" value="<?=!empty($user_meta['gstin'])?$user_meta['gstin']:''?>"> 
-              <br>
-            </div>
-        </div>
-    
-    </div>    
-</div>
-<div class="col-md-4">
-  <div class="Yorder">
-    <table style="width: 100%;">
-      <tr>
-        <th>Your order</th>
-        <th>Price with GST</th>                
-      </tr>
-      <?php
-      $cartarr = $this->cart->contents();
-      if (!empty($cartarr)) {        
-        foreach($cartarr as $ind => $cart) {
-          $gst  = !empty($cart['gst'])?$cart['gst']:'0';
-          ?>
-          <tr>
-            <td><?=$cart['name']?> x <?=$cart['qty']?>(Qty)</td>
-            <td>₹ <?=$cart['price']*$cart['qty'].'('.$gst.'%)'?></td>
-          </tr>
-          <?php
-        }
-      }
-      ?>
-      <tr>
-        <td>Subtotal</td>        
-        <td>₹ <?=$this->cart->total()?></td>
-      </tr>
-      
-    </table>
-    <br>
-    <label class="part_payment"><input type="checkbox" name="part_payment" value="1"> Part Payment</label>
-    <div class="part_payment_amount">
-      <label>Amount : ₹</label>
-      <input type="number" name="part_payment_amount" style="width: 30%;" min="10" max="<?=$this->cart->total()?>">
-    </div>
-    <br>
-     <div>
-      <input type="radio" name="dbt" value="1"> Cash on Delivery
-    </div> 
-    <div>
-      <input type="radio" name="dbt" value="2" checked> Online <span>
-      <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg" alt="" width="50">
-      </span>
-    </div>
+          </div>  
 
-    <button type="submit" class="checkout-form-button">Place Order</button>
-  </div><!-- Yorder -->
- </div>
- </div>
+
+        <div class="panel panel-default" style="border: 4px solid #53d4fa;" id="shipping-form">
+          <div class="panel-heading no-print" style="background-color: #53d4fa;">
+              Shipping Details              
+          </div>
+          <div class="panel-body panel-form" >
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="form-group">        
+                        <label>Full Name <i class="required">*</i></label>
+                        <input type="text" name="shipping_fname" class="form-control">
+                    </div>        
+                </div>       
+                
+                <div class="col-md-12">
+                   <div class="form-group">        
+                    <label>Address <i class="required">*</i></label>
+                    <input type="text" name="shipping_address" placeholder="House number and street name with zipcode" class="form-control" >
+                  </div>          
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">        
+                    <label> State <i class="required">*</i></label>
+                    <select name="shipping_state" id="state" class="form-control" required>
+                      <option value="">Select State</option>
+                      <?php foreach($user_state as $state){ ?>
+                        <option value="<?php echo $state->id; ?>"><?php echo $state->state; ?></option>
+                      <?php } ?>
+                    </select> 
+                  </div>
+                </div>        
+                <div class="col-md-6">
+                  <div class="form-group">        
+                  <label> City <i class="required">*</i></label>
+                  <select name="shipping_city" id="city" class="form-control" required>
+                    <option value="">Select City</option>
+                    <?php foreach($user_city as $city){ ?>
+                      <option value="<?php echo $city->id; ?>"><?php echo $city->city; ?></option>
+                    <?php } ?>
+                  </select> 
+                  </div>
+                </div>
+               
+                <div class="col-md-6">
+                  <div class="form-group">        
+                    <label>Postcode / ZIP <i class="required">*</i></label>
+                    <input type="text" name="shipping_pincode" class="form-control" > 
+                  </div>
+                </div>        
+                <div class="col-md-6">
+                    <div class="form-group">
+                    <label>Mobile no. <i class="required">*</i></label>
+                    <input type="tel" name="shipping_phone" class="form-control" > 
+                  </div>
+                </div>        
+                <div class="col-md-6">
+                    <div class="form-group">    
+                      <label>Email Address <i class="required">*</i></label>
+                      <input type="email" name="shipping_email" class="form-control"> 
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="Yorder" style="border: 4px solid #53d4fa;">
+          <table style="width: 100%;">
+            <tr>
+              <th>Your order</th>
+              <th>Price with GST</th>                
+            </tr>
+            <?php
+            $cartarr = $this->cart->contents();
+            if (!empty($cartarr)) {        
+              foreach($cartarr as $ind => $cart) {
+                $gst  = !empty($cart['gst'])?$cart['gst']:'0';
+                ?>
+                <tr>
+                  <td><?=$cart['name']?> x <?=$cart['qty']?>(Qty)</td>
+                  <td>₹ <?=$cart['price']*$cart['qty'].'('.$gst.'%)'?></td>
+                </tr>
+                <?php
+              }
+            }
+            ?>
+            <tr>
+              <td>Subtotal</td>        
+              <td>₹ <?=$this->cart->total()?></td>
+            </tr>
+            
+          </table>
+          <br>
+          <label class="part_payment"><input type="checkbox" name="part_payment" value="1"> Part Payment</label>
+          <div class="part_payment_amount">
+            <label>Amount : ₹</label>
+            <input type="number" name="part_payment_amount" style="width: 30%;" min="10" max="<?=$this->cart->total()?>">
+          </div>
+          <br>
+           <div>
+            <input type="radio" name="dbt" value="1"> Cash on Delivery
+          </div> 
+          <div>
+            <input type="radio" name="dbt" value="2" checked> Online <span>
+            <img src="https://www.logolynx.com/images/logolynx/c3/c36093ca9fb6c250f74d319550acac4d.jpeg" alt="" width="50">
+            </span>
+          </div>
+
+          <button type="submit" class="checkout-form-button">Place Order</button>
+        </div><!-- Yorder -->
+       </div>
+    </div>
 </div>
   </form>
 <script type="text/javascript">
@@ -257,11 +315,12 @@ p{
 
  <script type="text/javascript">
     $(function () {
+        $("#shipping-form").hide(800);
         $("#chkshipping").click(function () {
             if ($(this).is(":checked")) {
-                $("#dvship").show();
+                $("#shipping-form").show(800);
             } else {
-                $("#dvship").hide();
+                $("#shipping-form").hide(800);
             }
         });
     });
