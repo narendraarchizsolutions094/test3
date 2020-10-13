52e7323b4024ee7a98e85eade9faab101701ea32
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-10-13 00:40:59 -0500

    product list

--- application/views/product/prod-det-list.php
+++ application/views/product/prod-det-list.php
@@ -82,7 +82,7 @@
                      }
                      ?></td>
                      <td data-th='Minimum Order Quantity'><?=$row->minimum_order_quantity?></td>                     
-                     <td data-th='Seller'><a href="<?=base_url().'client/view/'.$row->enquiry_id?>"><?=!empty($row->seller)?$row->seller:'NA'?></a></td>
+                     <td data-th='Seller'><a href="<?=base_url().'client/view/'.$row->enquiry_id?>"><?=($row->seller!='')?$row->seller:'NA'?></a></td>
             				 <td data-th='Seller Mobile'><a href="tel:<?=$row->seller_phone?>"><?=$row->seller_phone?></a></td>
             				 <td data-th='Status'> 
                       <?php                      
