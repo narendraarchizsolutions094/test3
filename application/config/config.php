f0e244595805037f45886b5b590ddfc0a7caa895
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-10-23 02:24:30 -0500

    test

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,8 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
+ini_set('session.cookie_samesite', 'None');
+ini_set('session.cookie_secure', TRUE);
 $root=(isset($_SERVER["HTTPS"]) ? "https://" : "http://").$_SERVER["HTTP_HOST"];
 $root.= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
 $config["base_url"] = $root; 
