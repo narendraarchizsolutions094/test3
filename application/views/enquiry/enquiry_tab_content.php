8230bcdd9e3ae7726f7a8dfa3442f4be0a3c0ff1
suraj kumar <suraj@archiztechnologies.com>
2020-10-29 20:01:46 +0530

    user list,cart,checkout,home,header,product

--- application/views/enquiry/enquiry_tab_content.php
+++ application/views/enquiry/enquiry_tab_content.php
@@ -60,12 +60,10 @@ if (user_access(450)) { ?>
       <input class="form-control" value="<?php echo $details->lastname ?>" name="lastname" type="text" placeholder="Last Name" >  
    </div>
    <?php }?>
-   <?php
-      if(is_active_field(MOBILE,$process_id)){
-      ?>
+   <?php  if(is_active_field(MOBILE,$process_id)){  ?>
    <div class="form-group col-sm-6 col-md-6"> 
       <label><?php echo display('mobile') ?></label>
-      <input class="form-control mask-number" name="mobileno" type="password" maxlength='10' value="<?php echo $details->phone ?>" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" >
+      <input class="form-control mask-number" name="mobileno" type="text" maxlength='10' value="<?php echo $details->phone ?>" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" >
       <i class="fa fa-plus" onclick="add_more_phone('add_more_phone')" style="float:right;margin-top:-25px;margin-right:10px;color:red"></i>
    </div>
    <?php }?>
