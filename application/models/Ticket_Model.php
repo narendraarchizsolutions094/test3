e2f9b8ef5b47fb696c78d5f15127b36109ee070f
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-11-21 19:01:35 -0500

    attachment issue

--- application/models/Ticket_Model.php
+++ application/models/Ticket_Model.php
@@ -141,10 +141,10 @@ class Ticket_Model extends CI_Model
 			"referred_by"   => ($this->input->post("referred_by", true)) ? $this->input->post("referred_by", true) : "",
 
 		);
-		if (empty($_FILES["attachment"]["name"]) && $_FILES["attachment"]["size"][0] > 0) {
+		if (!empty($_FILES["attachment"]["name"]) && $_FILES["attachment"]["size"][0] > 0) {
 			$retdata =  $this->do_upload();
-			print_r($retdata);			
-			exit();
+			// print_r($retdata);			
+			// exit();
 			if (!empty($retdata)) {
 				if (isset($_POST["ticketno"])) {
 					$old_ticket =  $this->db->where(array('id' => $_POST['ticketno']))->get('tbl_ticket')->row();
