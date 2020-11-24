a9c77826f01d673e5909424ef817fd73b1602a7a
Shivam Gautam <shivamgautam@archiztechnologies.com>
2020-11-24 21:03:31 +0530

    enquiry tab api

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -502,12 +502,12 @@ class Enquiry extends CI_Controller
         }
     }
 
-    public function test()
-    {$this->load->model('Enquiry_model');
-       $prefix=  $this->Leads_Model->get_leadsource_list();
-        //$basic= $this->location_model->get_company_list1(197);
-        print_r($prefix);
-    }
+    // public function test()
+    // {$this->load->model('Enquiry_model');
+    //    $prefix=  $this->Enquiry_model->getEnquiry(array('enquiry_id'=>'192874'))->result();
+    //     //$basic= $this->location_model->get_company_list1(197);
+    //     print_r($prefix);
+    // }
 
 
     public function create()
