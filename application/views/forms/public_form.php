fc0aabb72fff974efd4f0b6dbe1ef12c6707e0cc
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-11-25 20:11:53 -0500

    test

--- application/views/forms/public_form.php
+++ application/views/forms/public_form.php
@@ -15,6 +15,11 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-bar-rating/1.2.1/themes/fontawesome-stars.min.css">        
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-bar-rating/1.2.1/jquery.barrating.min.js"></script>        
+
 </head>
   <body class="bg-light">
     <div class="container">
@@ -180,3 +185,11 @@
 </div>
 </body>
 </html>
+
+<script>
+  $(function() {
+    $('.star-rating').barrating({
+      theme: 'fontawesome-stars'
+    });  
+  });
+</script>
