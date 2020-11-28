652a7115de3e7e5e0cff45ad1cd527eeac282e3d
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-11-27 22:04:05 -0500

    test

--- application/views/setting/create-document-template.php
+++ application/views/setting/create-document-template.php
@@ -65,11 +65,57 @@
 </div>
 <?php } ?>
   <script>
-    tinymce.init({
-      selector: 'textarea',
-      plugins: 'quickbars,lists advlist',
-      toolbar: 'true',
-      toolbar_mode: 'floating',
-      });
+    // tinymce.init({
+    //   selector: 'textarea',
+    //   plugins: 'quickbars,lists advlist',
+    //   toolbar: 'true',
+    //   toolbar_mode: 'floating',
+    //   });
       
+      
+var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+tinymce.init({
+  selector: 'textarea',
+  plugins: 'print preview powerpaste casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable',
+  mobile: {
+    plugins: 'print preview powerpaste casechange importcss searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker textpattern noneditable help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable'
+  },
+  menu: {
+    tc: {
+      title: 'TinyComments',
+      items: 'addcomment showcomments deleteallconversations'
+    }
+  },
+  menubar: 'file edit view insert format tools table tc help',
+  toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+  autosave_ask_before_unload: true,
+  autosave_interval: '30s',
+  autosave_prefix: '{path}{query}-{id}-',
+  autosave_restore_when_empty: false,
+  autosave_retention: '2m',
+  image_advtab: true,
+  importcss_append: true,
+  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+  height: 600,
+  image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  noneditable_noneditable_class: 'mceNonEditable',
+  toolbar_mode: 'sliding',
+  spellchecker_whitelist: ['Ephox', 'Moxiecode'],
+  tinycomments_mode: 'embedded',
+  content_style: '.mymention{ color: gray; }',
+  contextmenu: 'link image imagetools table configurepermanentpen',
+  a11y_advanced_options: true,
+  skin: useDarkMode ? 'oxide-dark' : 'oxide',
+  content_css: useDarkMode ? 'dark' : 'default',
+  mentions_selector: '.mymention',
+  mentions_fetch: mentions_fetch,
+  mentions_menu_hover: mentions_menu_hover,
+  mentions_menu_complete: mentions_menu_complete,
+  mentions_select: mentions_select
+});
+
+
   </script>
