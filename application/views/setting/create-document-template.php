1ee5b4e58ecf651fdf6d2ddbf7a966a47fa94f32
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-11-27 22:29:53 -0500

    test

--- application/views/setting/create-document-template.php
+++ application/views/setting/create-document-template.php
@@ -35,7 +35,7 @@
 
                                                            <label for="name" class="col-form-label">Content</label>
                                                            <!-- <textarea name="content" class="form-control" required></textarea> -->
-                                    <textarea name="content" class="tinymce form-control" placeholder="" id="degree" maxlength="140" rows="7"><?= $value->content; ?></textarea>
+                                    <textarea name="content" class="tinymce2 form-control" placeholder="" id="degree" maxlength="140" rows="7"><?= $value->content; ?></textarea>
 
                                                            <!-- <textarea name="content" class="form-control" id="summernote" required><?= $value->content; ?></textarea> -->
 
