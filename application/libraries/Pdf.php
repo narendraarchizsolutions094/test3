bd174d6dfbacaae87cc11eef74adc0ec037d39f2
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-11-30 19:53:26 -0500

    test

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -57,7 +57,7 @@ public function load_view($view, $data = array(),$pdfFilePath1)
   
     // Output the generated PDF to Browser
     //$pdf_string =   $dompdf->output($pdfFilePath1, 'F');
-    $pdf = $dompdf->output(['isRemoteEnabled' => true]);
+    $pdf = $dompdf->output();
     $file_location = $pdfFilePath1;
     file_put_contents($file_location,$pdf); 
     $dompdf->output("quotation.pdf");
