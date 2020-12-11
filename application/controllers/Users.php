da8a504ffb986751bcae88db36ad0c951679ff31
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-11 17:18:15 -0500

    reporting locations

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class User extends CI_Controller {
+class Users extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
