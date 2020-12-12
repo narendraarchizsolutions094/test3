e40cd5711c7b23e987fc63b2b7d8ae50760b8004
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-11 19:59:03 -0500

    test

--- application/views/chats/chats.php
+++ application/views/chats/chats.php
@@ -558,10 +558,11 @@
 					 $this->db->select("*");
 					 $this->db->from('tbl_admin');        
 					 $this->db->join('tbl_user_role', 'tbl_user_role.use_id=tbl_admin.user_permissions', 'left');        
-                     $this->db->join("chat", "receiver_id = '$user_id'", "LEFT");
+                     $this->db->join("chat", "chat.receiver_id = tbl_admin.pk_i_admin_id", "LEFT");
 					 $this->db->where('tbl_admin.companey_id',$this->session->companey_id); 
                      $this->db->where('tbl_admin.b_status',1);                              
                      $this->db->order_by('chat.message_date_time','DESC');	                  
+                     $this->db->group_by('tbl_admin.pk_i_admin_id');
                      
                      $all_user=$this->db->get()->result();
 					 if(!empty($all_user)){
