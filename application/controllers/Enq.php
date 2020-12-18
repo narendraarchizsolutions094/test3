efb6d1846ff9003423d0e3b7b3c4deed0ce75319
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-18 15:58:12 -0500

    test

--- application/controllers/Enq.php
+++ application/controllers/Enq.php
@@ -44,6 +44,13 @@ class Enq extends CI_Controller
 		$data['products'] = $this->dash_model->get_user_product_list();
 		$data['drops'] = $this->enquiry_model->get_drop_list();
 		$data['all_stage_lists'] = $this->Leads_Model->get_leadstage_list_byprocess1($this->session->process,1);
+		
+		if($this->session->user_id == 286){
+			echo "<pre>";
+			print_r($data['all_stage_lists']);
+			echo "</pre>";
+		}
+		
 		$data['prodcntry_list'] = $this->enquiry_model->get_user_productcntry_list();
 		$data['state_list'] = $this->enquiry_model->get_user_state_list();
 		$data['city_list'] = $this->enquiry_model->get_user_city_list();
