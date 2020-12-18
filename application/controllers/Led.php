90370e7f062b6042c17d87b14f20c0d4075d9e7e
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-18 16:01:13 -0500

    test

--- application/controllers/Led.php
+++ application/controllers/Led.php
@@ -29,7 +29,14 @@ class Led extends CI_Controller {
 		$data['datasourse'] = $this->report_model->all_datasource();
         $data['drops'] 		= $this->enquiry_model->get_drop_list();		
 	    //$data['all_stage_lists'] = $this->Leads_Model->find_stage();
-        $data['all_stage_lists'] = $this->Leads_Model->get_leadstage_list_byprocess1($this->session->process,2);
+		$data['all_stage_lists'] = $this->Leads_Model->get_leadstage_list_byprocess1($this->session->process,2);
+		
+		if($this->session->user_id == 286){
+			echo "<pre>";
+			print_r($data['all_stage_lists']);
+			echo "</pre>";
+		}
+		
 		$data['lead_score'] = $this->enquiry_model->get_leadscore_list();	
 		$data['created_bylist'] = $this->User_model->user_list();	
 		$data['data_type'] = 2;
