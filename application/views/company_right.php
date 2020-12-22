1735dcfe0ed81742d5b83cf7a42a86152451e036
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-21 20:09:44 -0500

    test

--- application/views/company_right.php
+++ application/views/company_right.php
@@ -6,7 +6,7 @@
             <div class="panel-heading no-print">
             </div>
             <div class="panel-body">
-                <form action="<?php echo base_url('user/update_user_role')?>" method="post">
+                
                     <div class="form-group row">
                         <label for="inputText"
                             class="col-sm-1 col-form-label"><?php echo display('user_function') ?></label>
@@ -49,7 +49,7 @@
                     <!-- <div class="row text-center">
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div> -->
-                </form>
+                
             </div>
         </div>
     </div>
