7fb04f7a3e9a5bd5351b43a32b0bcdf3d04a166f
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2020-12-31 15:11:52 -0500

    test

--- application/views/ticket/ticket_disposition.php
+++ application/views/ticket/ticket_disposition.php
@@ -204,7 +204,7 @@ if(!empty($ticket->ticket_stage)){
 if($this->session->companey_id == 65){ ?>
   <script>
   if(<?=$ticket->complaint_type?>==2){
-    $("#lead_stage_change").change();
+    $("select[name=lead_description]").load("<?=base_url('message/find_substage/2')?>");    
     $("#lead_stage_change").val('1');
 
     $("select[name='ticket_status']").val('3');
