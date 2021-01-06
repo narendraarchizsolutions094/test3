5733d803f30e77a2cd49332ffd3c6885d7081046
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-01-06 14:14:48 -0500

    lang api

---
+++ application/controllers/api/Setting.php
@@ -0,0 +1,22 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+class Setting extends REST_Controller {
+    function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function get_all_labels(){
+        $comp_id = $this->input->post('comp_id');
+        $this->db->where('comp_id',$comp_id);
+        $this->db->or_where('comp_id',0);
+        $lang    =  $this->db->get('language')->result_array();
+  
+        $this->set_response([
+            'status' => false,
+            'message' => $lang  
+           ], REST_Controller::HTTP_OK);  
+    }
+} 
