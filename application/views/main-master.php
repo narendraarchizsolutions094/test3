b9624c7274dd8317f9003fd38fcd729240bb2729
Shivam Gautam <shivamgautam@archiztechnologies.com>
2021-01-07 16:38:27 +0530

    master

---
+++ application/views/main-master.php
@@ -0,0 +1,474 @@
+<div class="row" style="padding: 15px;">
+
+	<div class="col-lg-12 search-box">
+		<label class="s_i"><i class="fa fa-search"></i></label>
+		<input type="search" name="" class="pull-right" placeholder="Search for a setting like 'location' or 'ticket'" id="search_ip">
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Location
+			<br>
+			<small>All settings related to location</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('location/create')?>">
+					<img src="https://img.icons8.com/carbon-copy/40/000000/worldwide-location.png"/> Add Country
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6" >
+				<a href="<?=base_url('location/region')?>">
+					<img src="https://img.icons8.com/ios/50/000000/singapore-territory.png"/> Add Region
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6" style="    padding: 4px 15px;">
+				<a href="<?=base_url('location/state')?>">
+					<img src="https://img.icons8.com/wired/30/000000/empty-flag.png"/> Add State
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('location/territory')?>">
+					<img src="https://img.icons8.com/wired/64/000000/country.png"/> Add Territory
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('location/city')?>">
+					<img src="https://img.icons8.com/wired/64/000000/city.png"/> Add City
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('location/import')?>">
+					<img src="https://img.icons8.com/wired/64/000000/import.png"/> Import
+				</a>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="col-lg-12 box">
+		<p>Sales
+			<br>
+			<small>All settings related to Sales</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('leadRules/leadrules')?>">
+					<img src="https://img.icons8.com/wired/64/000000/rules.png"/> Add Lead Rule
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6" >
+				<a href="<?=base_url('lead/stage')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/leaderboard.png"/>     Add Lead Stage
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6" style="    padding: 4px 15px;">
+				<a href="<?=base_url('lead/description')?>">
+					<img src="https://img.icons8.com/windows/32/000000/category.png"/> Add Lead Description
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/score')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/average-value.png"/> Conversion Probability
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/lead_source')?>">
+					<img src="https://img.icons8.com/pastel-glyph/64/000000/warehouse-1.png"/> Add Source
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/subsourcelist')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/warehouse.png"/> Add Sub Source
+				</a>
+			</div>
+			
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/add_drop')?>">
+					<img src="https://img.icons8.com/wired/64/000000/delete-sign.png"/> Add Drop Reason
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/load_customer_channel_mater')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/customer-insight.png"/> Add Enquiry Type
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/product-list')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/process.png"/> Add Process
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/productcountry')?>">
+					<img src="https://img.icons8.com/wired/64/000000/product.png"/> Add Product/Service
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/institutelist')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/admission.png"/> Add Institue
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/crslist')?>">
+					<img src="https://img.icons8.com/wired/64/000000/courses.png"/> Add Course
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/sub_course')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/syllabus.png"/> Add Sub Course
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/taskstatuslist')?>">
+					<img src="https://img.icons8.com/wired/64/000000/task--v3.png"/> Add Task Status
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('lead/centerlist')?>">
+					<img src="https://img.icons8.com/ios/50/000000/data-center--v2.png"/> Add Center
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>E-commerce
+			<br>
+			<small>All settings related to E-commerce</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('warehouse/warehouse')?>">
+					<img src="https://img.icons8.com/wired/64/000000/warehouse.png"/> Add Warehouse
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('warehouse/typeofproduct')?>">
+					<img src="https://img.icons8.com/wired/64/000000/new-product.png"/> Add Product Type
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('scheme')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/overview-pages-1.png"/> Add Scheme
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('product/measurement_unit')?>">
+					<img src="https://img.icons8.com/ios/50/000000/ruler.png"/> Add Measurement Unit
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('warehouse/inventory')?>">
+					<img src="https://img.icons8.com/ios/50/000000/inventory-flow.png"/> Inventory
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('warehouse/inventory')?>">
+					<img src="https://img.icons8.com/wired/64/000000/import-csv.png"/> Upload Inventory
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>User Management
+			<br>
+			<small>All settings related to User Management</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('user/user_type')?>">
+					<img src="https://img.icons8.com/ios/50/000000/user-rights.png"/> User Rights
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('user/index')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/list.png"/> User List
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('user/user_tree')?>">
+					<img src="https://img.icons8.com/pastel-glyph/64/000000/parallel-tasks.png"/> User Hierarchy
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>API Configuration
+			<br>
+			<small>All settings related to API Configuration</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('whatsappapi')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/whatsapp--v1.png"/> Whatsapp
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('smsapi')?>">
+					<img src="https://img.icons8.com/ios/50/000000/sms.png"/> SMS
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('emailapi')?>">
+					<img src="https://img.icons8.com/wired/64/000000/email.png"/> Email
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('configurations/index')?>">
+					<img src="https://img.icons8.com/wired/64/000000/domain.png"/> Website
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('configurations/qr_code')?>">
+					<img src="https://img.icons8.com/wired/64/000000/qr-code.png"/> QR-Code
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Reports
+			<br>
+			<small>All settings related to Reports</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('report')?>">
+					<img src="https://img.icons8.com/wired/64/000000/total-sales.png"/> Sales Report
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('report/ticket_report')?>">
+					<img src="https://img.icons8.com/ios/50/000000/ticket-confirmed.png"/> Ticket Report
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('call_report/index')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/phone-not-being-used.png"/> Telephony Report
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>User Activity
+			<br>
+			<small>All settings related to user activity</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('attendance/logs')?>">
+					<img src="https://img.icons8.com/cotton/64/000000/raise-a-hand-to-answer--v1.png"/> Attendance
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Knowledge Base
+			<br>
+			<small>All settings related to knowledge</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('knowledge_base/index')?>">
+					<img src="https://img.icons8.com/wired/64/000000/knowledge-sharing.png"/> Knowledge Base
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('knowledge_base/articles')?>">
+					<img src="https://img.icons8.com/wired/64/000000/news.png"/> Article
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('knowledge_base/category')?>">
+					<img src="https://img.icons8.com/metro/26/000000/hot-article.png"/> Category
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Inovice
+			<br>
+			<small>All settings related to knowledge</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('invoice/invoice/index')?>">
+					<img src="https://img.icons8.com/ios/50/000000/invoice-1.png"/> Invoice List
+				</a>
+			</div>
+
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('invoice/invoice/create')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/invoice-1.png"/> Create Invoice
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('invoice/invoice/report')?>">
+					<img src="https://img.icons8.com/dotty/80/000000/cash-receipt.png"/>Inovice Report
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('invoice/invoice/settings')?>">
+					<img src="https://img.icons8.com/carbon-copy/100/000000/gear.png"/>Inovice Setting
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Language
+			<br>
+			<small>All settings related to Language</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('language/index')?>">
+					<img src="https://img.icons8.com/wired/64/000000/language.png"/> All Language
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('language/phrase')?>">
+					<img src="https://img.icons8.com/ios/50/000000/lettering.png"/> Add Phrase
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12 box">
+		<p>Ticket
+			<br>
+			<small>All settings related to Ticket</small>
+		</p>
+		<div class="row">
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('ticket/index')?>">
+					<img src="https://img.icons8.com/windows/32/000000/ticket-confirmed.png"/> Ticket
+				</a>
+			</div>
+			<div class="col-lg-3 col-sm-4 col-xs-6">
+				<a href="<?=base_url('ticket/add')?>">
+					<img src="https://img.icons8.com/ios/24/000000/add-ticket.png"/> Add Ticket
+				</a>
+			</div>
+		</div>
+	</div>
+
+</div>
+<style type="text/css">
+.content{
+	background:#f1f3f6;
+}
+.box{
+	background: white;
+	border-radius: 4px;
+	box-shadow: 1px 2px 5px 1px #f4f4f4;
+	padding: 15px 20px;
+	margin-top:8px; 
+	margin-bottom:8px;
+}
+.box p
+{
+	font-size: 18px;
+	border-bottom: 1px dashed #cbc8c8;
+}
+.box p small
+{
+	font-size: 12px;
+}
+.box>.row
+{
+	margin-left: -15px;
+	margin-right: -15px;
+}
+.box img
+{
+	max-height: 40px;
+}
+
+.box .col-lg-3
+{
+	margin-top: 11px;
+	margin-bottom:11px;
+}
+
+.search-box
+{
+	background: white;
+	border-radius: 4px;
+	box-shadow: 1px 2px 5px 1px #f4f4f4;
+	padding: 15px 15px 12px 15px;
+	margin-top:8px; 
+	margin-bottom:8px;
+}
+.search-box i
+{
+	color : #939393;
+}
+.search-box input
+{
+	width:97%;
+	border: 0px;
+	outline: 0;
+
+}
+.box a
+{
+	color:inherit;
+}
+.box a:hover
+{
+	filter: drop-shadow(2px 4px 3px gray);
+}
+</style>
+<script type="text/javascript">
+	try
+	{
+	$("#search_ip").on('keyup',function(){
+
+		$(".s_i").html("<i class='fa fa-spinner fa-spin'></i>");
+
+		var key = this.value.toLowerCase();
+	
+		var box_list = $(".content").find('.box');
+		
+		$(box_list).each(function(k,v){
+
+			var sub = $(v).find('.col-lg-3');
+			var chk = 0;
+			$(sub).each(function(a,b){
+					 var text = $(b).first('a').text().toLowerCase();
+
+					if(text.includes(key)){
+						$(b).show();
+						chk = 1;
+					}
+					else
+						$(b).hide();
+			});
+
+			if(chk)
+				$(v).show();
+			else
+				$(v).hide();
+
+		});
+		$(".s_i").html("<i class='fa fa-search'></i>");
+	});
+}catch(e){alert(e);}
+</script>
