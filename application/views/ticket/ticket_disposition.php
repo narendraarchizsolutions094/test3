5ab33d696500ae0df18d674bd5960a7ef63c586f
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-01-07 23:39:48 -0500

    test

--- application/views/ticket/ticket_disposition.php
+++ application/views/ticket/ticket_disposition.php
@@ -203,8 +203,10 @@ if(!empty($ticket->ticket_stage)){
 <?php
 if($this->session->companey_id == 65){ ?>
   <script>
-  $("select[name='ticket_status']").on('change',function(){    
-    has_close_authority(false);
+  $("select[name='ticket_status']").on('change',function(){  
+    if(confirm('Are you sure ?')){
+      has_close_authority(false);
+    }  
   });
 
   function has_close_authority(auto=true){
