163bf13ddf82430f1b5bff5e653968e21ca26826
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-01-08 11:47:58 -0500

    load rule model

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -2592,6 +2592,7 @@ class Ticket extends CI_Controller
 				if($this->db->update('tbl_ticket')){
 					$comment_id = $this->Ticket_Model->saveconv($tid,'Ticket Status Changed','', $this->input->post('client'),$this->session->user_id,0,0,$status,$comp_id);
 					$ticketno = $this->input->post('ticketno');
+					$this->load->model('rule_model');
 					$this->rule_model->execute_rules($ticketno, array(3,6,7));
 				}
 			}
